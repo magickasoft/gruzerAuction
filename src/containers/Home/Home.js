a681fe9398dce79505c8d9357d95485f72a2caae
Evgeny Shmakov <sjs-master@yandex.ru>
2018-09-19 03:31:49 +0700

    add icons for both platform

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -5,6 +5,10 @@ import config from 'config';
 import { getMessage } from 'actions/webMessages';
 
 class Home extends PureComponent {
+  componentDidMount(){
+    // console.log('this.webView', this.webView);
+    // setTimeout(() => console.log('this.webView', this.webView), 6000);
+  }
   render() {
 
     return (
